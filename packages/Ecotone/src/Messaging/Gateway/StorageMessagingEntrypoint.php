ad72d0319c90c28214695677c610ae1a99fd7d41
Dariusz Gafka <dariuszgafka@gmail.com>
2022-08-31 20:31:03 +0200

    Fix phpstan

--- packages/Ecotone/src/Messaging/Gateway/StorageMessagingEntrypoint.php
+++ packages/Ecotone/src/Messaging/Gateway/StorageMessagingEntrypoint.php
@@ -2,7 +2,6 @@
 
 namespace Ecotone\Messaging\Gateway;
 
-use Ecotone\Messaging\Attribute\MessageGateway;
 use Ecotone\Messaging\Attribute\Parameter\Header;
 use Ecotone\Messaging\Attribute\Parameter\Headers;
 use Ecotone\Messaging\Attribute\Parameter\Payload;
@@ -22,16 +21,16 @@ class StorageMessagingEntrypoint implements MessagingEntrypoint
 
     public function send(#[Payload] $payload, #[Header(MessagingEntrypoint::ENTRYPOINT)] string $targetChannel): mixed
     {
-        // TODO: Implement send() method.
+        return null;
     }
 
     public function sendWithHeaders(#[Payload] $payload, #[Headers] array $headers, #[Header(MessagingEntrypoint::ENTRYPOINT)] string $targetChannel): mixed
     {
-        // TODO: Implement sendWithHeaders() method.
+        return null;
     }
 
     public function sendMessage(Message $message): mixed
     {
-        // TODO: Implement sendMessage() method.
+        return null;
     }
 }

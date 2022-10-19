416c9d78f3635d023dd570f3b084d007e69a5710
Dariusz Gafka <dariuszgafka@gmail.com>
2022-10-19 18:02:20 +0200

    Fix laravel provider

--- packages/Laravel/src/EcotoneProvider.php
+++ packages/Laravel/src/EcotoneProvider.php
@@ -166,7 +166,7 @@ class EcotoneProvider extends ServiceProvider
 
         $this->app->singleton(
             ConfiguredMessagingSystem::class,
-            new LazyConfiguredMessagingSystem($this->app)
+            function () {return new LazyConfiguredMessagingSystem($this->app);}
         );
 
         $this->app->singleton(

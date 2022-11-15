f8976298590079c5f08c7d986399e3cc0460967f
Dariusz Gafka <dariuszgafka@gmail.com>
2022-11-15 20:46:53 +0100

    handle destination failure

--- packages/Ecotone/tests/Lite/Test/EcotoneTestSupportTest.php
+++ packages/Ecotone/tests/Lite/Test/EcotoneTestSupportTest.php
@@ -7,6 +7,7 @@ use Ecotone\Lite\Test\TestConfiguration;
 use Ecotone\Lite\Test\TestSupportGateway;
 use Ecotone\Messaging\Config\ModulePackageList;
 use Ecotone\Messaging\Config\ServiceConfiguration;
+use Ecotone\Messaging\Handler\DestinationResolutionException;
 use PHPUnit\Framework\TestCase;
 use Test\Ecotone\Modelling\Fixture\Order\ChannelConfiguration;
 use Test\Ecotone\Modelling\Fixture\Order\OrderService;
@@ -129,7 +130,7 @@ final class EcotoneTestSupportTest extends TestCase
         $this->assertEmpty($testSupportGateway->getSentCommandMessages());
     }
 
-    public function test_not_failing_in_test_mode_when_no_routing_command_found()
+    public function test_not_command_bus_failing_in_test_mode_when_no_routing_command_found()
     {
         $ecotoneTestSupport = EcotoneTestSupport::boostrapWithMessageHandlers(
             [\Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService::class],
@@ -145,5 +146,56 @@ final class EcotoneTestSupportTest extends TestCase
         $ecotoneTestSupport->getCommandBus()->sendWithRouting('basket.addItem', $command);
 
         $this->assertEquals([$command], $ecotoneTestSupport->getTestSupportGateway()->getSentCommands());
+    }
+
+    public function test_failing_command_bus_in_test_mode_when_no_routing_command_found()
+    {
+        $ecotoneTestSupport = EcotoneTestSupport::boostrapWithMessageHandlers(
+            [\Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService::class],
+            [new \Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService()],
+            ServiceConfiguration::createWithDefaults()
+                ->withEnvironment("test")
+                ->withExtensionObjects([
+                    TestConfiguration::createWithDefaults()->withFailOnCommandHandlerNotFound(true)
+                ]),
+        );
+
+        $this->expectException(DestinationResolutionException::class);
+
+        $ecotoneTestSupport->getCommandBus()->sendWithRouting('basket.addItem', new PlaceOrder("someId"));
+    }
+
+    public function test_not_query_bus_failing_in_test_mode_when_no_routing_command_found()
+    {
+        $ecotoneTestSupport = EcotoneTestSupport::boostrapWithMessageHandlers(
+            [\Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService::class],
+            [new \Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService()],
+            ServiceConfiguration::createWithDefaults()
+                ->withEnvironment("test")
+                ->withExtensionObjects([
+                    TestConfiguration::createWithDefaults()->withFailOnQueryHandlerNotFound(false)
+                ]),
+        );
+
+        $ecotoneTestSupport->getQueryBus()->sendWithRouting('basket.getItem', new \stdClass());
+
+        $this->assertEquals([new \stdClass()], $ecotoneTestSupport->getTestSupportGateway()->getSentQueries());
+    }
+
+    public function test_failing_query_bus_in_test_mode_when_no_routing_command_found()
+    {
+        $ecotoneTestSupport = EcotoneTestSupport::boostrapWithMessageHandlers(
+            [\Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService::class],
+            [new \Test\Ecotone\Modelling\Fixture\MetadataPropagating\OrderService()],
+            ServiceConfiguration::createWithDefaults()
+                ->withEnvironment("test")
+                ->withExtensionObjects([
+                    TestConfiguration::createWithDefaults()->withFailOnQueryHandlerNotFound(true)
+                ]),
+        );
+
+        $this->expectException(DestinationResolutionException::class);
+
+        $ecotoneTestSupport->getQueryBus()->sendWithRouting('basket.addItem', new PlaceOrder("someId"));
     }
 }

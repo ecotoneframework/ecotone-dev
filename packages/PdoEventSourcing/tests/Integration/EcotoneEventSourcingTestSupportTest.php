131298668eea95da5af43ca19c61330ca2555b53
Dariusz Gafka <dariuszgafka@gmail.com>
2022-11-19 15:55:48 +0100

    async test for projection

--- packages/PdoEventSourcing/tests/Integration/EcotoneEventSourcingTestSupportTest.php
+++ packages/PdoEventSourcing/tests/Integration/EcotoneEventSourcingTestSupportTest.php
@@ -12,6 +12,7 @@ use Test\Ecotone\EventSourcing\Fixture\Ticket\Command\RegisterTicket;
 use Test\Ecotone\EventSourcing\Fixture\Ticket\Ticket;
 use Test\Ecotone\EventSourcing\Fixture\Ticket\TicketEventConverter;
 use Test\Ecotone\EventSourcing\Fixture\TicketWithInMemoryAsynchronousEventDrivenProjection\InProgressTicketList;
+use Test\Ecotone\EventSourcing\Fixture\TicketWithInMemoryAsynchronousEventDrivenProjection\ProjectionConfiguration;
 
 final class EcotoneEventSourcingTestSupportTest extends TestCase
 {
@@ -32,6 +33,29 @@ final class EcotoneEventSourcingTestSupportTest extends TestCase
         $this->assertCount(0, $ecotoneTestSupport->getQueryBus()->sendWithRouting('getInProgressTickets'));;
 
         $ecotoneTestSupport->getCommandBus()->send(new RegisterTicket("1", "johny", "alert"));
+
+        $this->assertCount(1, $ecotoneTestSupport->getQueryBus()->sendWithRouting('getInProgressTickets'));;
+    }
+
+    public function test_registering_with_asynchronous_package()
+    {
+        $ecotoneTestSupport = EcotoneTestSupport::boostrapWithEventSourcing(
+            [Ticket::class, TicketEventConverter::class, InProgressTicketList::class, ProjectionConfiguration::class],
+            [new TicketEventConverter(), new InProgressTicketList()],
+            ServiceConfiguration::createWithDefaults()
+                ->withEnvironment("test")
+                ->withExtensionObjects([
+                    EventSourcingConfiguration::createInMemory(),
+                    TestConfiguration::createWithDefaults()
+                ]),
+            enableModulePackages: [ModulePackageList::EVENT_SOURCING_PACKAGE, ModulePackageList::ASYNCHRONOUS_PACKAGE]
+        );
+
+        $ecotoneTestSupport->getCommandBus()->send(new RegisterTicket("1", "johny", "alert"));
+
+        $this->assertCount(0, $ecotoneTestSupport->getQueryBus()->sendWithRouting('getInProgressTickets'));;
+
+        $ecotoneTestSupport->run('asynchronous_projections');
 
         $this->assertCount(1, $ecotoneTestSupport->getQueryBus()->sendWithRouting('getInProgressTickets'));;
     }

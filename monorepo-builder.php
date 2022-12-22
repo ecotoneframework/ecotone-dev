7d01feead1ee3ac253c3cb2a927b2098aa88e50a
Dariusz Gafka <dariuszgafka@gmail.com>
2022-12-22 23:40:00 +0100

    update

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Monorepo\SetCurrentMutualDependenciesReleaseWorker;
-use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
 use Symplify\MonorepoBuilder\Config\MBConfig;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\AddTagToChangelogReleaseWorker;
 use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;

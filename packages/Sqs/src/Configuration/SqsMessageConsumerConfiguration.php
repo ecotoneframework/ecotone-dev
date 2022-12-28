4cb838eb442988c9cabdc97c469636be117f50ff
Dariusz Gafka <dgafka.mail@gmail.com>
2022-12-28 08:31:14 +0100

    additional functions for sqs (#71)

--- packages/Sqs/src/Configuration/SqsMessageConsumerConfiguration.php
+++ packages/Sqs/src/Configuration/SqsMessageConsumerConfiguration.php
@@ -3,12 +3,27 @@
 namespace Ecotone\Sqs\Configuration;
 
 use Ecotone\Enqueue\EnqueueMessageConsumerConfiguration;
+use Ecotone\Sqs\SqsInboundChannelAdapterBuilder;
 use Enqueue\Sqs\SqsConnectionFactory;
 
 final class SqsMessageConsumerConfiguration extends EnqueueMessageConsumerConfiguration
 {
+    private bool $declareOnStartup = SqsInboundChannelAdapterBuilder::DECLARE_ON_STARTUP_DEFAULT;
+
     public static function create(string $endpointId, string $queueName, string $amqpConnectionReferenceName = SqsConnectionFactory::class): self
     {
         return new self($endpointId, $queueName, $amqpConnectionReferenceName);
+    }
+
+    public function withDeclareOnStartup(bool $declareOnStartup): self
+    {
+        $this->declareOnStartup = $declareOnStartup;
+
+        return $this;
+    }
+
+    public function isDeclaredOnStartup(): bool
+    {
+        return $this->declareOnStartup;
     }
 }

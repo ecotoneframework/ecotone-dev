851d5f21d0887c2e50a281e8d841b2ef6670a868
Dariusz Gafka <dariuszgafka@gmail.com>
2023-04-16 14:40:39 +0200

    Count failure handling as message consumption

--- packages/Amqp/tests/Behat/Bootstrap/DomainContext.php
+++ packages/Amqp/tests/Behat/Bootstrap/DomainContext.php
@@ -194,6 +194,8 @@ class DomainContext extends AmqpMessagingTest implements Context
                 false
             );
         }
+
+        $this->queueCleanUp();
     }
 
     /**
@@ -371,6 +373,7 @@ class DomainContext extends AmqpMessagingTest implements Context
 
     /**
      * @When using :serviceName process ticket with failure
+     * @When using :serviceName set up distributed consumer
      */
     public function usingProcessTicketWithFailure(string $serviceName)
     {

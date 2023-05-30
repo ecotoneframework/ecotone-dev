ea1482c118bee34ce8bbfcbc54475e5371d9a9d1
Dariusz Gafka <dgafka.mail@gmail.com>
2023-05-30 21:17:41 +0200

    add php benchmarks (#131)
    * add php benchmarks
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes

---
+++ packages/Laravel/tests/Application/Benchmark/EcotoneBenchmark.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Test\Ecotone\Laravel\Application\Benchmark;
+
+use Ecotone\Messaging\Config\ConfiguredMessagingSystem;
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Http\Kernel;
+use PHPUnit\Framework\TestCase;
+
+class
+EcotoneBenchmark extends TestCase
+{
+    /**
+     * @Revs(10)
+     * @Iterations(5)
+     * @Warmup(1)
+     */
+    public function bench_kernel_boot_on_prod()
+    {
+        putenv("APP_ENV=production");
+        $this->createApplication();
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(5)
+     * @Warmup(1)
+     */
+    public function bench_kernel_boot_on_dev()
+    {
+        putenv('APP_ENV=development');
+        $this->createApplication();
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(5)
+     * @Warmup(1)
+     */
+    public function bench_messaging_boot_on_prod()
+    {
+        putenv('APP_ENV=production');
+        $app = $this->createApplication();
+        $app->make(ConfiguredMessagingSystem::class);
+    }
+
+    /**
+     * @Revs(10)
+     * @Iterations(5)
+     * @Warmup(1)
+     */
+    public function bench_messaging_boot_on_dev()
+    {
+        putenv('APP_ENV=development');
+        $app = $this->createApplication();
+        $app->make(ConfiguredMessagingSystem::class);
+    }
+
+    public function createApplication(): Application
+    {
+        $app = require __DIR__ . '/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+}

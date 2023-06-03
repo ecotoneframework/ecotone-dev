6220329f4941be6d02b764d94350ddbb694efeb9
jlabedo <jean@needelp.com>
2023-06-03 10:47:45 +0200

    ci: run benchmarks with diff

--- bin/run-benchmarks.php
+++ bin/run-benchmarks.php
@@ -7,24 +7,35 @@ use PhpBench\PhpBench;
 use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 require __DIR__ . "/../vendor/autoload.php";
 
+$profilesToBenchmark = [
+    'ecotone',
+    'symfony',
+    'laravel'
+];
+
 $commandInput = new ArgvInput(definition: new InputDefinition([
-    new InputArgument('output', InputArgument::OPTIONAL)
+    new InputArgument('output', InputArgument::OPTIONAL),
+    new InputOption('--baseline', null, InputOption::VALUE_NONE, 'generate baseline'),
 ]));
 $outputToStream = $commandInput->getArgument('output');
+$generateBaseline = $commandInput->getOption('baseline');
 
 $console = new ConsoleOutput();
 $buffer = new BufferedOutput();
 $buffer->writeln("# PR stats");
 PhpBenchExtension::setDefaultOutput($buffer);
-foreach (['ecotone', 'symfony', 'laravel'] as $profile) {
+foreach ($profilesToBenchmark as $profile) {
     $buffer->writeln("<details><summary>$profile benchmarks</summary>");
-    $input = new StringInput("run --profile=$profile --report=aggregate --ref=main");
+    $input = $generateBaseline
+        ? new StringInput("run --profile=$profile --report=aggregate --tag=main")
+        : new StringInput("run --profile=$profile --report=aggregate --ref=main");
 
     $container = PhpBench::loadContainer($input);
 

cec01a227dd49d71b384398fddd9f350d04cd0be
Dariusz Gafka <dariuszgafka@gmail.com>
2023-06-05 17:06:43 +0200

    Adjust the test

--- packages/PdoEventSourcing/tests/Integration/PollingProjectionTest.php
+++ packages/PdoEventSourcing/tests/Integration/PollingProjectionTest.php
@@ -6,6 +6,8 @@ namespace Test\Ecotone\EventSourcing\Integration;
 
 use Ecotone\Lite\EcotoneLite;
 use Ecotone\Messaging\Config\ServiceConfiguration;
+use Ecotone\Messaging\Endpoint\ExecutionPollingMetadata;
+use Ecotone\Messaging\Endpoint\PollingMetadata;
 use PHPUnit\Framework\TestCase;
 use Test\Ecotone\EventSourcing\Fixture\Basket\BasketEventConverter;
 use Test\Ecotone\EventSourcing\Fixture\Basket\Command\AddProduct;
@@ -17,20 +19,20 @@ final class PollingProjectionTest extends TestCase
 {
     public function test_running_polling_projection(): void
     {
-        $ecotoneLite = EcotoneLite::bootstrapFlowTesting(
-            classesToResolve: [BasketListConfiguration::class],
+        $ecotoneLite = EcotoneLite::bootstrapFlowTestingWithEventStore(
+            classesToResolve: [BasketListConfiguration::class, BasketList::class],
             containerOrAvailableServices: [new BasketList(), new BasketEventConverter()],
             configuration: ServiceConfiguration::createWithDefaults()
                 ->withNamespaces(['Test\Ecotone\EventSourcing\Fixture\Basket'])
         );
 
         $ecotoneLite->sendCommand(new CreateBasket('1000'));
-        $ecotoneLite->run(BasketList::PROJECTION_NAME);
+        $ecotoneLite->run(BasketList::PROJECTION_NAME, ExecutionPollingMetadata::createWithTestingSetup(maxExecutionTimeInMilliseconds: 1000));
 
         self::assertEquals(['1000' => []], $ecotoneLite->sendQueryWithRouting('getALlBaskets'));
 
         $ecotoneLite->sendCommand(new AddProduct('1000', 'milk'));
-        $ecotoneLite->run(BasketList::PROJECTION_NAME);
+        $ecotoneLite->run(BasketList::PROJECTION_NAME, ExecutionPollingMetadata::createWithTestingSetup(maxExecutionTimeInMilliseconds: 1000));
 
         self::assertEquals(['1000' => ['milk']], $ecotoneLite->sendQueryWithRouting('getALlBaskets'));
     }

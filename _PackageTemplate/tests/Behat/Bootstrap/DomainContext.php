aaca6974a8dac746d92c64c7fec834a3c953c7a3
Piotr Zajac <piotr.zajac@unixslayer.pl>
2023-06-12 18:05:29 +0200

    dropping behat tests in DBAL Module by rewriting them with Ecotone Test Support

---
+++ _PackageTemplate/tests/Behat/Bootstrap/DomainContext.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Test\Ecotone\_PackageTemplate\Behat\Bootstrap;
+
+use Behat\Behat\Context\Context;
+use PHPUnit\Framework\TestCase;
+
+class DomainContext extends TestCase implements Context
+{
+
+}

975b84057cdd274f448666e7578acba1d397251f
jlabedo <jean@needelp.com>
2023-07-16 19:11:07 +0200

    Poll consumer while there are messages to consume

--- packages/Ecotone/src/Messaging/Endpoint/InterceptedConsumer.php
+++ packages/Ecotone/src/Messaging/Endpoint/InterceptedConsumer.php
@@ -3,6 +3,7 @@
 namespace Ecotone\Messaging\Endpoint;
 
 use Ecotone\Messaging\Endpoint\Interceptor\ConnectionExceptionRetryInterceptor;
+use Ecotone\Messaging\Endpoint\Interceptor\FinishWhenNoMessagesInterceptor;
 use Ecotone\Messaging\Endpoint\Interceptor\LimitConsumedMessagesInterceptor;
 use Ecotone\Messaging\Endpoint\Interceptor\LimitExecutionAmountInterceptor;
 use Ecotone\Messaging\Endpoint\Interceptor\LimitMemoryUsageInterceptor;
@@ -95,6 +96,9 @@ class InterceptedConsumer implements ConsumerLifecycle
         }
         if ($pollingMetadata->getExecutionTimeLimitInMilliseconds() > 0) {
             $interceptors[] = new TimeLimitInterceptor($pollingMetadata->getExecutionTimeLimitInMilliseconds());
+        }
+        if ($pollingMetadata->finishWhenNoMessages()) {
+            $interceptors[] = new FinishWhenNoMessagesInterceptor();
         }
         $interceptors[] = new ConnectionExceptionRetryInterceptor($referenceSearchService->get(LoggingHandlerBuilder::LOGGER_REFERENCE), $pollingMetadata->getConnectionRetryTemplate(), $pollingMetadata->isStoppedOnError());
 
--- packages/Ecotone/tests/Messaging/Unit/Endpoint/Poller/PollingConsumerBuilderTest.php
+++ packages/Ecotone/tests/Messaging/Unit/Endpoint/Poller/PollingConsumerBuilderTest.php
@@ -17,8 +17,10 @@ use Ecotone\Messaging\Handler\InMemoryReferenceSearchService;
 use Ecotone\Messaging\Handler\InterfaceToCallRegistry;
 use Ecotone\Messaging\Handler\Recoverability\RetryTemplateBuilder;
 use Ecotone\Messaging\Handler\ServiceActivator\ServiceActivatorBuilder;
+use Ecotone\Messaging\Message;
 use Ecotone\Messaging\MessageHeaders;
 use Ecotone\Messaging\MessagingException;
+use Ecotone\Messaging\PollableChannel;
 use Ecotone\Messaging\Support\MessageBuilder;
 use InvalidArgumentException;
 use RuntimeException;
@@ -391,6 +393,60 @@ class PollingConsumerBuilderTest extends MessagingTest
 
         $this->assertTrue($acknowledgementCallback->isRequeued());
         $this->assertNull($errorChannel->receive());
+    }
+
+    public function test_finish_when_no_messages(): void
+    {
+        $inputChannelName = 'inputChannel';
+        $inputChannel = new class implements PollableChannel {
+            private array $queue = [];
+
+            public function receive(): ?Message
+            {
+                return array_shift($this->queue);
+            }
+
+            public function send(Message $message): void
+            {
+                $this->queue[] = $message;
+            }
+
+            public function receiveWithTimeout(int $timeoutInMilliseconds): ?Message
+            {
+                return $this->receive();
+            }
+
+            public function queueSize(): int
+            {
+                return count($this->queue);
+            }
+        };
+        $objectToInvokeOn = new class {
+            public array $receivedMessages = [];
+            public function handle(Message $message): void
+            {
+                $this->receivedMessages[] = $message;
+            }
+        };
+        $messageHandlerBuilder = ServiceActivatorBuilder::createWithDirectReference($objectToInvokeOn, 'handle')
+            ->withEndpointId('some-id')
+            ->withInputChannelName($inputChannelName);
+
+        $pollingConsumer = $this->createPollingConsumerWithCustomConfiguration(
+            [
+                $inputChannelName => $inputChannel,
+            ],
+            $messageHandlerBuilder,
+            PollingMetadata::create('some')
+                ->setFinishWhenNoMessages(true)
+        );
+
+        $inputChannel->send(MessageBuilder::withPayload('some')->build());
+        $inputChannel->send(MessageBuilder::withPayload('some')->build());
+        $pollingConsumer->run();
+
+        $this->assertSame(0, $inputChannel->queueSize());
+        $this->assertCount(2, $objectToInvokeOn->receivedMessages);
     }
 
     private function createPollingConsumer(string $inputChannelName, QueueChannel $inputChannel, $messageHandler): \Ecotone\Messaging\Endpoint\ConsumerLifecycle

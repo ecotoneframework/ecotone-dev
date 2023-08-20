93601f5e9c5a4ae16bf3566974615805d6b9abfa
dgafka <dgafka@users.noreply.github.com>
2023-08-20 10:52:51 +0000

    Release 1.101.0

--- packages/Ecotone/src/Messaging/Attribute/BusinessMethod.php
+++ packages/Ecotone/src/Messaging/Attribute/BusinessMethod.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Ecotone\Messaging\Attribute;
 
 use Attribute;
-use Ecotone\Messaging\Handler\Gateway\GatewayProxyBuilder;
 
 #[Attribute(Attribute::TARGET_METHOD)]
 class BusinessMethod extends MessageGateway
 {
-
 }

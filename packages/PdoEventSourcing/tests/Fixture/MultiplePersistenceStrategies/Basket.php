1bfcf280ae658f1d6074652e40188690324cda9c
Piotr Zając <piotr.zajac@unixslayer.pl>
2023-09-24 19:17:50 +0200

    Allow to define custom persistence strategy per stream. (#203)
    Allow to define custom persistence strategy per stream. If custom strategy is not defined, the one default will be used.
    
    resolves #202

---
+++ packages/PdoEventSourcing/tests/Fixture/MultiplePersistenceStrategies/Basket.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Ecotone\EventSourcing\Fixture\MultiplePersistenceStrategies;
+
+use Ecotone\EventSourcing\Attribute\AggregateType;
+use Ecotone\EventSourcing\Attribute\Stream;
+use Ecotone\Modelling\Attribute\EventSourcingAggregate;
+use Ecotone\Modelling\Attribute\EventSourcingHandler;
+use Ecotone\Modelling\Attribute\Identifier;
+use Ecotone\Modelling\WithAggregateVersioning;
+
+#[EventSourcingAggregate]
+#[AggregateType(self::AGGREGATE_TYPE)]
+#[Stream(self::STREAM)]
+final class Basket
+{
+    use WithAggregateVersioning;
+
+    public const AGGREGATE_TYPE = 'basket';
+    public const STREAM = 'basket';
+
+    #[Identifier]
+    public string $basketId;
+
+    #[EventSourcingHandler]
+    public function applyBasketCreated(BasketCreated $event): void
+    {
+        $this->basketId = $event->basketId;
+    }
+}
---
+++ packages/PdoEventSourcing/tests/Fixture/MultiplePersistenceStrategies/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Ecotone\EventSourcing\Fixture\MultiplePersistenceStrategies;
+
+use Ecotone\EventSourcing\Attribute\AggregateType;
+use Ecotone\EventSourcing\Attribute\Stream;
+use Ecotone\Modelling\Attribute\EventSourcingAggregate;
+use Ecotone\Modelling\Attribute\EventSourcingHandler;
+use Ecotone\Modelling\Attribute\Identifier;
+use Ecotone\Modelling\WithAggregateVersioning;
+
+#[EventSourcingAggregate]
+#[AggregateType(self::AGGREGATE_TYPE)]
+#[Stream(self::STREAM)]
+final class Order
+{
+    use WithAggregateVersioning;
+
+    public const AGGREGATE_TYPE = 'order';
+    public const STREAM = 'order';
+
+    #[Identifier]
+    public string $orderId;
+
+    #[EventSourcingHandler]
+    public function applyOrderCreated(OrderCreated $event): void
+    {
+        $this->orderId = $event->orderId;
+    }
+}

2ec8214d3fadeda6ad80f1274af2ac25cf50d1c3
Dariusz Gafka <dgafka.mail@gmail.com>
2023-10-03 08:42:52 +0200

    Warmup Ecotone cache in symfony (#218)
    * Warmup Ecotone cache in symfony
    
    * preload

---
+++ packages/Symfony/DepedencyInjection/Compiler/CacheWarmer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Ecotone\SymfonyBundle\DepedencyInjection\Compiler;
+
+use Ecotone\Messaging\Config\ConfiguredMessagingSystem;
+use Ecotone\Messaging\Config\MessagingSystemConfiguration;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\HttpKernel\CacheClearer\CacheClearerInterface;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+
+class CacheWarmer implements CacheWarmerInterface
+{
+    public function __construct(private ContainerInterface $containerInterface) {}
+
+    public function isOptional()
+    {
+        return false;
+    }
+
+    public function warmUp(string $cacheDir)
+    {
+        $this->containerInterface->get(ConfiguredMessagingSystem::class)->list();
+
+        return [];
+    }
+}

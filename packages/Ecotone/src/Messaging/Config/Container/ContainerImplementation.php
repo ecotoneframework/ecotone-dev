2d7ec1e76ede3c709d067f9050febcf872b07f36
jlabedo <jean@needelp.com>
2023-10-08 15:42:33 +0200

    removing the ContainerHydrator thing
    this is handled in the framework

--- packages/Ecotone/src/Messaging/Config/Container/ContainerImplementation.php
+++ packages/Ecotone/src/Messaging/Config/Container/ContainerImplementation.php
@@ -5,8 +5,6 @@ namespace Ecotone\Messaging\Config\Container;
 interface ContainerImplementation
 {
     public const REFERENCE_ID = 'compiled_container';
-    public const EXTERNAL_REFERENCE_SEARCH_SERVICE_ID = 'external_reference_search_service';
-    public const EXTERNAL_CHANNEL_RESOLVER_SERVICE_ID = 'channelResolver_legacy';
 
     /**
      * @param array<string, Definition> $definitions

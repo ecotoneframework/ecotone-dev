a79fbdd7e343b14c71a3b09ca8b291cb8c19a71f
Dariusz Gafka <dariuszgafka@gmail.com>
2023-10-22 15:19:39 +0200

    More reliable tests

--- Monorepo/Benchmark/FullAppBenchmarkCase.php
+++ Monorepo/Benchmark/FullAppBenchmarkCase.php
@@ -100,23 +100,17 @@ abstract class FullAppBenchmarkCase extends TestCase
 
     public static function clearLiteCache(): void
     {
-        self::productionEnvironments();
-        MessagingSystemConfiguration::cleanCache(
-            new ServiceCacheConfiguration(
-                __DIR__ . '/../ExampleApp/Lite/var/cache',
-                true
-            )
+        self::deleteFiles(
+            __DIR__ . '/../ExampleApp/Lite/var/cache',
+            false
         );
     }
 
     public static function clearLiteApplicationCache(): void
     {
-        self::productionEnvironments();
-        MessagingSystemConfiguration::cleanCache(
-            new ServiceCacheConfiguration(
-                __DIR__ . '/../ExampleApp/LiteApplication/var/cache',
-                true
-            )
+        self::deleteFiles(
+            __DIR__ . '/../ExampleApp/LiteApplication/var/cache',
+            false
         );
     }
 
@@ -134,28 +128,43 @@ abstract class FullAppBenchmarkCase extends TestCase
 
     public static function clearLaravelCache(): void
     {
-        self::productionEnvironments();
-        MessagingSystemConfiguration::cleanCache(
-            new ServiceCacheConfiguration(
-                __DIR__ . '/../ExampleApp/Laravel/storage/framework/cache/data',
-                true
-            )
+        self::deleteFiles(
+            __DIR__ . '/../ExampleApp/Laravel/storage/framework/cache/data',
+            false
         );
     }
 
     public static function clearSymfonyCache(): void
     {
-        self::productionEnvironments();
-        $kernel = self::bootSymfonyKernel(environment: 'prod', debug: false);
-        $input = [];
-        $commandName = 'cache:clear';
+        self::deleteFiles(
+            __DIR__ . '/../ExampleApp/Symfony/var/cache',
+            false
+        );
+    }
 
-        self::executeSymfonyConsoleCommand($kernel, $commandName, $input);
+    private static function deleteFiles(string $target, bool $deleteDirectory): void
+    {
+        if (is_dir($target)) {
+            \Ecotone\Messaging\Support\Assert::isTrue(
+                is_writable($target),
+                "Not enough permissions to delete from cache directory {$target}"
+            );
+            $files = glob($target . '*', GLOB_MARK);
 
-        self::developmentEnvironments();
-        $kernel = self::bootSymfonyKernel(environment: 'dev', debug: true);
+            foreach ($files as $file) {
+                self::deleteFiles($file, true);
+            }
 
-        self::executeSymfonyConsoleCommand($kernel, $commandName, $input);
+            if ($deleteDirectory) {
+                rmdir($target);
+            }
+        } elseif (is_file($target)) {
+            Assert::isTrue(
+                is_writable($target),
+                "Not enough permissions to delete cache file {$target}"
+            );
+            unlink($target);
+        }
     }
 
     public abstract function executeForSymfony(

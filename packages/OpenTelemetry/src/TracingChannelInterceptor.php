28e28cc4ed1a0183a8887bed8fc21505e4e873cf
Dariusz Gafka <dariuszgafka@gmail.com>
2023-11-04 14:37:20 +0100

    Fixes to avoid Notices from opentelemetry

--- packages/OpenTelemetry/src/TracingChannelInterceptor.php
+++ packages/OpenTelemetry/src/TracingChannelInterceptor.php
@@ -9,6 +9,8 @@ use Ecotone\Messaging\Message;
 use Ecotone\Messaging\MessageChannel;
 use Ecotone\Messaging\Support\MessageBuilder;
 
+use OpenTelemetry\Context\ScopeInterface;
+use Ramsey\Uuid\Uuid;
 use function json_decode;
 use function json_encode;
 
@@ -25,6 +27,7 @@ use Throwable;
 final class TracingChannelInterceptor implements ChannelInterceptor
 {
     public const TRACING_CARRIER_HEADER = 'ecotoneTracingCarrier';
+    const ECOTONE_TEMPORARY_SPAN_CONTEXT_HEADER = 'ecotone.temporarySpanContext';
 
     public function __construct(private string $channelName, private TracerProviderInterface $tracerProvider)
     {
@@ -32,29 +35,48 @@ final class TracingChannelInterceptor implements ChannelInterceptor
 
     public function preSend(Message $message, MessageChannel $messageChannel): ?Message
     {
+//        Context::getCurrent()->attach($span->storeInContext(Context::getCurrent()));
+//        $span->activate();
+
+        $span1 = Span::getCurrent();
+        $context1 = Context::getCurrent();
+
         $span = EcotoneSpanBuilder::create($message, 'Sending to Channel: ' . $this->channelName, $this->tracerProvider, SpanKind::KIND_PRODUCER)
             ->startSpan();
-        $span->activate();
-
+//        $span2 = Span::getCurrent();
+        $scope = $span->activate();
+//        $span2 = Span::getCurrent();
+//        $context2 = Context::getCurrent();
         $ctx = $span->storeInContext(Context::getCurrent());
         $carrier = [];
         TraceContextPropagator::getInstance()->inject($carrier, null, $ctx);
+//        $context->detach();
+//
+//        $span3 = Span::getCurrent();
+//        $context3 = Context::getCurrent();
+//        $span->end();
+//
+//        $span4 = Span::getCurrent();
+//        $context4 = Context::getCurrent();
 
         return MessageBuilder::fromMessage($message)
                 ->setHeader(self::TRACING_CARRIER_HEADER, json_encode($carrier))
+                ->setHeader(self::ECOTONE_TEMPORARY_SPAN_CONTEXT_HEADER, $scope)
                 ->build();
     }
 
     public function postSend(Message $message, MessageChannel $messageChannel): void
     {
-
+// @TODO Remove header from message after notice are stopped from OpenTelemetry (https://github.com/open-telemetry/opentelemetry-php/issues/1138)
+        $currentContext = $message->getHeaders()->get(self::ECOTONE_TEMPORARY_SPAN_CONTEXT_HEADER);
+//        $currentContext = Context::storage()->scope();
+        $currentRelatedSpan = Span::getCurrent();
+        $currentContext->detach();
+        $currentRelatedSpan->end();
     }
 
     public function afterSendCompletion(Message $message, MessageChannel $messageChannel, ?Throwable $exception): bool
     {
-        Span::getCurrent()->end();
-        Context::storage()->scope()->detach();
-
         return false;
     }
 

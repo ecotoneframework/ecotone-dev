c4828d64d529eedd3630d6ddb8ed8bc466974bb2
Dariusz Gafka <dariuszgafka@gmail.com>
2023-11-11 11:20:53 +0100

    make use of connection from the configuration

--- packages/Dbal/src/EcotoneManagerRegistryConnectionFactory.php
+++ packages/Dbal/src/EcotoneManagerRegistryConnectionFactory.php
@@ -4,22 +4,31 @@ declare(strict_types=1);
 
 namespace Ecotone\Dbal;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\Persistence\ManagerRegistry;
 use Enqueue\Dbal\ManagerRegistryConnectionFactory;
 
 final class EcotoneManagerRegistryConnectionFactory extends ManagerRegistryConnectionFactory
 {
     private ManagerRegistry $registry;
+    /** @var array<string, string> */
+    private array $config;
 
     public function __construct(ManagerRegistry $registry, array $config = [])
     {
         parent::__construct($registry, $config);
 
         $this->registry = $registry;
+        $this->config = $config;
     }
 
     public function getRegistry(): ManagerRegistry
     {
         return $this->registry;
+    }
+
+    public function getConnection(): Connection
+    {
+        return $this->registry->getConnection($this->config['connection_name'] ?? null);
     }
 }

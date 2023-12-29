35a8a2afa5fb7d2d1d7d210d013c10b15dcbc7c5
Dariusz Gafka <dgafka.mail@gmail.com>
2023-12-29 07:43:33 +0100

    Business interface examples (#283)
    * Business interface examples
    
    * examples
    
    * example
    
    * examples

---
+++ quickstart-examples/BusinessInterface/Routing/run_example.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\WorkingWithAggregateDirectly\Command\ChangePrice;
+use App\WorkingWithAggregateDirectly\Command\RegisterProduct;
+use App\WorkingWithAggregateDirectly\Product;
+use App\WorkingWithAggregateDirectly\ProductRepository;
+use App\WorkingWithAggregateDirectly\ProductService;
+use Ecotone\Lite\EcotoneLiteApplication;
+use Enqueue\Dbal\DbalConnectionFactory;
+use PHPUnit\Framework\Assert;
+use Ramsey\Uuid\Uuid;
+
+require __DIR__ . "/vendor/autoload.php";
+$messagingSystem = EcotoneLiteApplication::boostrap([DbalConnectionFactory::class => new DbalConnectionFactory(getenv('DATABASE_DSN') ? getenv('DATABASE_DSN') : 'pgsql://ecotone:secret@localhost:5432/ecotone')], pathToRootCatalog: __DIR__);
+
+$cacheService = $messagingSystem->getGatewayByName(\App\BusinessInterface\CacheService::class);
+
+$cacheService->set(
+    new \App\BusinessInterface\CachedItem("pageViews", "12333", "data"),
+    \App\BusinessInterface\CacheType::IN_MEMORY
+);
+
+Assert::assertEquals("12333", $cacheService->get("pageViews", \App\BusinessInterface\CacheType::IN_MEMORY));
+Assert::assertNull($cacheService->get("pageViews", \App\BusinessInterface\CacheType::FILE_SYSTEM));
+echo "Cache set and get successfully\n";

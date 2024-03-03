78cb336b8767e0a33c200f40ee91844ef765c0ef
Dariusz Gafka <dariuszgafka@gmail.com>
2024-03-03 11:03:30 +0100

    type

--- packages/JmsConverter/tests/Fixture/ExamplesToConvert/PropertyWithAnnotationMetadataDefined.php
+++ packages/JmsConverter/tests/Fixture/ExamplesToConvert/PropertyWithAnnotationMetadataDefined.php
@@ -10,6 +10,7 @@ class PropertyWithAnnotationMetadataDefined
      * @Serializer\SerializedName("naming")
      * @Serializer\Type("string")
      */
+    #[Serializer\SerializedName("naming")]
     private $name;
 
     /**

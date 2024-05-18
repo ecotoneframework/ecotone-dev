2a85c9b9490a6c86916b632e00807ec4591ca3df
Dariusz Gafka <dgafka.mail@gmail.com>
2024-05-18 16:18:37 +0200

    Allow to use target aggregate id header for async scenarios (#323)
    * Allow to use target aggregate id header for async scenario

---
+++ packages/Ecotone/src/AnnotationFinder/FileSystem/RootCatalogNotFound.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ecotone\AnnotationFinder\FileSystem;
+
+use Ecotone\Messaging\Support\InvalidArgumentException;
+
+final class RootCatalogNotFound extends InvalidArgumentException
+{
+
+}

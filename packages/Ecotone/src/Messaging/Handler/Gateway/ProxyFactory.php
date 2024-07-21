a9bbdee207bf8a295d44025e11af37abf3308fa8
jlabedo <1191198+jlabedo@users.noreply.github.com>
2024-07-21 19:28:06 +0200

    proxy generation for Ecotone

--- packages/Ecotone/src/Messaging/Handler/Gateway/ProxyFactory.php
+++ packages/Ecotone/src/Messaging/Handler/Gateway/ProxyFactory.php
@@ -5,22 +5,10 @@ declare(strict_types=1);
 namespace Ecotone\Messaging\Handler\Gateway;
 
 use Ecotone\Messaging\Config\ConfiguredMessagingSystem;
+use Ecotone\Messaging\Config\Container\Definition;
 use Ecotone\Messaging\Config\Container\GatewayProxyReference;
-use Ecotone\Messaging\Config\EcotoneRemoteAdapter;
-use Ecotone\Messaging\Config\MessagingSystemConfiguration;
+use Ecotone\Messaging\Config\Container\Reference;
 use Ecotone\Messaging\Config\ServiceCacheConfiguration;
-use ProxyManager\Autoloader\AutoloaderInterface;
-use ProxyManager\Configuration;
-use ProxyManager\Factory\RemoteObject\AdapterInterface;
-use ProxyManager\Factory\RemoteObjectFactory;
-use ProxyManager\FileLocator\FileLocator;
-use ProxyManager\GeneratorStrategy\FileWriterGeneratorStrategy;
-use ProxyManager\Proxy\RemoteObjectInterface;
-use ProxyManager\Signature\ClassSignatureGenerator;
-use ProxyManager\Signature\SignatureGenerator;
-
-use function spl_autoload_register;
-use function spl_autoload_unregister;
 
 /**
  * Class LazyProxyConfiguration
@@ -32,76 +20,65 @@ use function spl_autoload_unregister;
  */
 class ProxyFactory
 {
-    private static ?AutoloaderInterface $registeredAutoloader = null;
-    private ?Configuration $configuration = null;
+    public const PROXY_NAMESPACE = "Ecotone\\__Proxy__";
+
+    private ProxyGenerator $proxyGenerator;
 
     public function __construct(private ServiceCacheConfiguration $serviceCacheConfiguration)
     {
+        $this->proxyGenerator = new ProxyGenerator(self::PROXY_NAMESPACE);
     }
 
-    private function getConfiguration(): Configuration
+    public static function getGatewayProxyDefinitionFor(GatewayProxyReference $proxyReference): Definition
     {
-        return $this->configuration ??= $this->buildConfiguration();
+        return new Definition(self::getClassNameFor($proxyReference->getInterfaceName()), [
+            new Definition(GatewayProxyReference::class, [
+                $proxyReference->getReferenceName(),
+                $proxyReference->getInterfaceName()
+            ]),
+            new Reference(ConfiguredMessagingSystem::class),
+        ], [self::class, "createProxyFor"]);
     }
 
-    private function buildConfiguration(): Configuration
+    public function createProxyFor(GatewayProxyReference $proxyReference, ConfiguredMessagingSystem $messagingSystem): object
     {
-        $configuration = new Configuration();
-
-        if ($this->serviceCacheConfiguration->shouldUseCache()) {
-            MessagingSystemConfiguration::prepareCacheDirectory($this->serviceCacheConfiguration);
-            $configuration->setProxiesTargetDir($this->serviceCacheConfiguration->getPath());
-            $fileLocator = new FileLocator($configuration->getProxiesTargetDir());
-            $configuration->setGeneratorStrategy(new FileWriterGeneratorStrategy($fileLocator));
-            $configuration->setClassSignatureGenerator(new ClassSignatureGenerator(new SignatureGenerator()));
+        $proxyClassName = self::getFullClassNameFor($proxyReference->getInterfaceName());
+        if (! class_exists($proxyClassName, false)) {
+            if ($this->serviceCacheConfiguration->shouldUseCache()) {
+                $file = $this->generateCachedProxyFileFor($proxyReference, false);
+                require $file;
+            } else {
+                $code = $this->proxyGenerator->generateProxyFor($proxyClassName, $proxyReference->getInterfaceName());
+                eval(\substr($code, 5));
+            }
         }
-
-        return $configuration;
+        return new $proxyClassName($messagingSystem, $proxyReference);
     }
 
-    private function createProxyClassWithAdapter(string $interfaceName, AdapterInterface $adapter): RemoteObjectInterface
+    public function generateCachedProxyFileFor(GatewayProxyReference $proxyReference, bool $overwrite): string
     {
-        $factory = new RemoteObjectFactory($adapter, $this->getConfiguration());
-        $this->registerProxyAutoloader();
-
-        return $factory->createProxy($interfaceName);
-    }
-
-    public static function createFor(string $referenceName, ConfiguredMessagingSystem $messagingSystem, string $interface, ServiceCacheConfiguration $serviceCacheConfiguration): object
-    {
-        $proxyFactory = new self($serviceCacheConfiguration);
-
-        return $proxyFactory->createProxyClassWithAdapter(
-            $interface,
-            new EcotoneRemoteAdapter($messagingSystem, new GatewayProxyReference($referenceName, $interface))
-        );
-    }
-
-    public function createWithCurrentConfiguration(string $referenceName, ConfiguredMessagingSystem $messagingSystem, string $interface): object
-    {
-        return self::createFor($referenceName, $messagingSystem, $interface, $this->serviceCacheConfiguration);
-    }
-
-    public function registerProxyAutoloader(): void
-    {
-        if (! $this->serviceCacheConfiguration->shouldUseCache()) {
-            return;
+        $proxyClassName = self::getFullClassNameFor($proxyReference->getInterfaceName());
+        $file = $this->getFilePathForProxy($proxyReference->getInterfaceName());
+        if ($overwrite || ! \file_exists($file)) {
+            $code = $this->proxyGenerator->generateProxyFor($proxyClassName, $proxyReference->getInterfaceName());
+            \file_put_contents($file, $code);
         }
+        return $file;
+    }
 
-        $autoloader = $this->getConfiguration()->getProxyAutoloader();
+    private function getFilePathForProxy(string $interfaceName) : string
+    {
+        $className = self::getClassNameFor($interfaceName);
+        return $this->serviceCacheConfiguration->getPath() . DIRECTORY_SEPARATOR . $className . ".php";
+    }
 
-        if (self::$registeredAutoloader === $autoloader) {
-            return;
-        }
+    private static function getClassNameFor(string $interfaceName): string
+    {
+        return \str_replace("\\", "_", $interfaceName);
+    }
 
-        if (self::$registeredAutoloader !== null) {
-            // another ProxyFactory instance may have already registered an autoloader.
-            // this should not happen normally, but just in case we will unload
-            // the old autoloader.
-            spl_autoload_unregister(self::$registeredAutoloader);
-        }
-
-        self::$registeredAutoloader = $autoloader;
-        spl_autoload_register(self::$registeredAutoloader);
+    private function getFullClassNameFor(string $interfaceName): string
+    {
+        return self::PROXY_NAMESPACE . "\\" . $this->getClassNameFor($interfaceName);
     }
 }

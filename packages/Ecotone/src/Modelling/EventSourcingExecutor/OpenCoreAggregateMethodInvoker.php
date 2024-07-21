b1a85873efbf16d7ed4334fdb50c3a2439a791c0
Dariusz Gafka <dariuszgafka@gmail.com>
2024-07-21 15:12:16 +0200

    clean up

--- packages/Ecotone/src/Modelling/EventSourcingExecutor/OpenCoreAggregateMethodInvoker.php
+++ packages/Ecotone/src/Modelling/EventSourcingExecutor/OpenCoreAggregateMethodInvoker.php
@@ -17,7 +17,7 @@ final class OpenCoreAggregateMethodInvoker implements AggregateMethodInvoker
     public function executeMethod(mixed $aggregate, InterfaceToCall $eventSourcingHandlerInterface, EventSourcingHandlerMethod $eventSourcingHandler, Message $message): void
     {
         if (count($eventSourcingHandlerInterface->getInterfaceParameters()) > 1) {
-            throw InvalidArgumentException::create("Using multiple parameters for {$eventSourcingHandlerInterface} is part of Enterprise features. To use this feature, obtain Enterprise.");
+            throw InvalidArgumentException::create("Using multiple parameters for Event Sourcing Handler: {$eventSourcingHandlerInterface} is part of Enterprise features. To use this feature obtain Enterprise.");
         }
 
         $aggregate->{$eventSourcingHandlerInterface->getMethodName()}($message->getPayload());

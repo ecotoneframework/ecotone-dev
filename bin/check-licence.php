87ba3b55e0ffa16725e8a59a6c229e444880a38e
Dariusz Gafka <dariuszgafka@gmail.com>
2024-11-04 20:21:31 +0100

    contribution files

---
+++ bin/check-licence.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * licence Apache-2.0
+ */
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$finder = new \Symfony\Component\Finder\Finder();
+
+$files = $finder->files()->in(__DIR__ . "/../packages/*/src")->name("*.php");
+
+foreach ($files as $file) {
+    $fileContent = file_get_contents($file->getRealPath());
+
+    if (! preg_match('/\*\s*(@licence|licence)\s+(Enterprise|Apache\-2\.0)\s*/', $fileContent, $matches)) {
+        throw new \RuntimeException("Missing licence in file: " . $file->getRealPath());
+    }
+}

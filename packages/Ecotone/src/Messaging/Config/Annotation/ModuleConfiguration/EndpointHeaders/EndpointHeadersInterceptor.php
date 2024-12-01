1e358c7d1f4c36e2556f3a725ae981a5adff4694
Dariusz Gafka <dariuszgafka@gmail.com>
2024-12-01 18:46:29 +0100

    expression based delaying message

--- packages/Ecotone/src/Messaging/Config/Annotation/ModuleConfiguration/EndpointHeaders/EndpointHeadersInterceptor.php
+++ packages/Ecotone/src/Messaging/Config/Annotation/ModuleConfiguration/EndpointHeaders/EndpointHeadersInterceptor.php
@@ -9,9 +9,18 @@ use Ecotone\Messaging\Attribute\Endpoint\Delayed;
 use Ecotone\Messaging\Attribute\Endpoint\Priority;
 use Ecotone\Messaging\Attribute\Endpoint\RemoveHeader;
 use Ecotone\Messaging\Attribute\Endpoint\TimeToLive;
+use Ecotone\Messaging\Config\ConfigurationException;
 use Ecotone\Messaging\Config\Container\DefinedObject;
 use Ecotone\Messaging\Config\Container\Definition;
+use Ecotone\Messaging\Config\Container\Reference;
+use Ecotone\Messaging\Handler\ExpressionEvaluationService;
+use Ecotone\Messaging\Handler\TypeDescriptor;
+use Ecotone\Messaging\Handler\UnionTypeDescriptor;
 use Ecotone\Messaging\MessageHeaders;
+use Ecotone\Messaging\Message;
+use Ecotone\Messaging\MessagingException;
+use Ecotone\Messaging\Scheduling\TimeSpan;
+use Nette\Utils\Type;
 
 /**
  * Class EndpointHeadersInterceptor
@@ -23,24 +32,74 @@ use Ecotone\Messaging\MessageHeaders;
  */
 class EndpointHeadersInterceptor implements DefinedObject
 {
-    public function addMetadata(?AddHeader $addHeader, ?Delayed $delayed, ?Priority $priority, ?TimeToLive $timeToLive, ?RemoveHeader $removeHeader): array
+    public function __construct(private ExpressionEvaluationService $expressionEvaluationService)
+    {
+
+    }
+
+    public function addMetadata(Message $message, ?AddHeader $addHeader, ?Delayed $delayed, ?Priority $priority, ?TimeToLive $timeToLive, ?RemoveHeader $removeHeader): array
     {
         $metadata = [];
 
         if ($addHeader) {
             $metadata[$addHeader->getHeaderName()] = $addHeader->getHeaderValue();
+
+            if ($addHeader->getExpression()) {
+                $metadata[$addHeader->getHeaderName()] = $this->expressionEvaluationService->evaluate($addHeader->getExpression(), [
+                    'payload' => $message->getPayload(),
+                    'headers' => $message->getHeaders()->headers()
+                ]);
+            }
         }
 
         if ($delayed) {
             $metadata[MessageHeaders::DELIVERY_DELAY] = $delayed->getHeaderValue();
+
+            if ($delayed->getExpression()) {
+                $metadata[MessageHeaders::DELIVERY_DELAY] = $this->expressionEvaluationService->evaluate($delayed->getExpression(), [
+                    'payload' => $message->getPayload(),
+                    'headers' => $message->getHeaders()->headers()
+                ]);
+            }
+
+            $type = TypeDescriptor::createFromVariable($metadata[MessageHeaders::DELIVERY_DELAY]);
+            if (!$type->isCompatibleWith(UnionTypeDescriptor::createWith([
+                TypeDescriptor::createIntegerType(),
+                TypeDescriptor::create(TimeSpan::class),
+                TypeDescriptor::create(\DateTimeInterface::class)
+            ]))) {
+                throw ConfigurationException::create("Delivery delay should be either integer, TimeSpan or DateTimeInterface, but got {$type->toString()}");
+            }
         }
 
         if ($priority) {
             $metadata[MessageHeaders::PRIORITY] = $priority->getHeaderValue();
+
+            if ($priority->getExpression()) {
+                $metadata[MessageHeaders::PRIORITY] = $this->expressionEvaluationService->evaluate($priority->getExpression(), [
+                    'payload' => $message->getPayload(),
+                    'headers' => $message->getHeaders()->headers()
+                ]);
+            }
         }
 
         if ($timeToLive) {
             $metadata[MessageHeaders::TIME_TO_LIVE] = $timeToLive->getHeaderValue();
+
+            if ($timeToLive->getExpression()) {
+                $metadata[MessageHeaders::TIME_TO_LIVE] = $this->expressionEvaluationService->evaluate($timeToLive->getExpression(), [
+                    'payload' => $message->getPayload(),
+                    'headers' => $message->getHeaders()->headers()
+                ]);
+            }
+
+            $type = TypeDescriptor::createFromVariable($metadata[MessageHeaders::TIME_TO_LIVE]);
+            if (!$type->isCompatibleWith(UnionTypeDescriptor::createWith([
+                TypeDescriptor::createIntegerType(),
+                TypeDescriptor::create(TimeSpan::class)
+            ]))) {
+                throw ConfigurationException::create("Delivery delay should be either integer or TimeSpan, but got {$type->toString()}");
+            }
         }
 
         if ($removeHeader) {
@@ -52,6 +111,8 @@ class EndpointHeadersInterceptor implements DefinedObject
 
     public function getDefinition(): Definition
     {
-        return new Definition(self::class);
+        return new Definition(self::class, [
+            Reference::to(ExpressionEvaluationService::REFERENCE)
+        ]);
     }
 }

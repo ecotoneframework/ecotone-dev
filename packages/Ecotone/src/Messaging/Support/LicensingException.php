08a7f849a4140abef1a78a654781e06855ae8272
Dariusz Gafka <dariuszgafka@gmail.com>
2025-05-10 18:04:55 +0200

    fixes

--- packages/Ecotone/src/Messaging/Support/LicensingException.php
+++ packages/Ecotone/src/Messaging/Support/LicensingException.php
@@ -13,7 +13,7 @@ final class LicensingException extends MessagingException
 {
     public static function create(string $message, ?int $errorCode = null): self
     {
-        return new static($message . " To purchase licence visist https://ecotone.tech.", is_null($errorCode) ? static::errorCode() : $errorCode);
+        return new static($message . " To purchase licence visit https://ecotone.tech.", is_null($errorCode) ? static::errorCode() : $errorCode);
     }
 
     protected static function errorCode(): int

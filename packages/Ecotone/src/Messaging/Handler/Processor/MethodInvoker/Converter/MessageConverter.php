d6d37dcb7d1bf5b0d95a9600cf5192293b87e647
Dariusz Gafka <dgafka.mail@gmail.com>
2025-06-14 22:18:27 +0200

    Allow Error Handler to execute asynchronously (#490)
    * Make error message serializable
    
    * allow to handle error messages asynchronously
    
    * fixes
    
    * fixes
    
    * fixes

--- packages/Ecotone/src/Messaging/Handler/Processor/MethodInvoker/Converter/MessageConverter.php
+++ packages/Ecotone/src/Messaging/Handler/Processor/MethodInvoker/Converter/MessageConverter.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Ecotone\Messaging\Handler\Processor\MethodInvoker\Converter;
 
 use Ecotone\Messaging\Handler\ParameterConverter;
+use Ecotone\Messaging\Handler\Recoverability\ErrorContext;
 use Ecotone\Messaging\Message;
+use Ecotone\Messaging\Support\ErrorMessage;
 
 /**
  * Class MessageArgument
@@ -28,6 +30,10 @@ class MessageConverter implements ParameterConverter
      */
     public function getArgumentFrom(Message $message): Message
     {
+        if (ErrorMessage::isErrorMessage($message)) {
+            return ErrorMessage::createFromMessage($message);
+        }
+
         return $message;
     }
 }

7e98e3c6241e9f9011bb0b89d42da2b1299dbd36
Dariusz Gafka <dariuszgafka@gmail.com>
2025-07-13 18:05:00 +0200

    fixes

--- packages/Ecotone/src/Messaging/Channel/PollableChannel/InMemory/InMemoryAcknowledgeStatus.php
+++ packages/Ecotone/src/Messaging/Channel/PollableChannel/InMemory/InMemoryAcknowledgeStatus.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Ecotone\Messaging\Channel\PollableChannel\InMemory;
 
+/**
+ * licence Apache-2.0
+ */
 enum InMemoryAcknowledgeStatus: int
 {
     case AWAITING = 0;

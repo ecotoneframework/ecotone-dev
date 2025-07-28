beaccac213085444aef7aeb23507d0d66153708d
Dariusz Gafka <dariuszgafka@gmail.com>
2025-07-28 20:46:27 +0200

    fix: only internal gateways to propagate routing slip

--- packages/Ecotone/src/Messaging/Handler/Gateway/ParameterToMessageConverter/GatewayHeadersBuilder.php
+++ packages/Ecotone/src/Messaging/Handler/Gateway/ParameterToMessageConverter/GatewayHeadersBuilder.php
@@ -5,10 +5,12 @@ namespace Ecotone\Messaging\Handler\Gateway\ParameterToMessageConverter;
 use Ecotone\Messaging\Attribute\BusinessMethod;
 use Ecotone\Messaging\Config\Container\Definition;
 use Ecotone\Messaging\Config\Container\MessagingContainerBuilder;
+use Ecotone\Messaging\Gateway\MessagingEntrypoint;
 use Ecotone\Messaging\Handler\Gateway\GatewayParameterConverterBuilder;
 use Ecotone\Messaging\Handler\InterfaceParameter;
 use Ecotone\Messaging\Handler\InterfaceToCall;
 use Ecotone\Messaging\Handler\TypeDescriptor;
+use Ecotone\Modelling\Attribute\Repository;
 use Ecotone\Modelling\CommandBus;
 use Ecotone\Modelling\EventBus;
 use Ecotone\Modelling\QueryBus;
@@ -58,18 +60,12 @@ class GatewayHeadersBuilder implements GatewayParameterConverterBuilder
 
     private function isStartingNewFlow(InterfaceToCall $interfaceToCall): bool
     {
-        foreach ([BusinessMethod::class] as $attribute) {
-            if ($interfaceToCall->hasAnnotation(TypeDescriptor::create($attribute))) {
-                return true;
-            }
-        }
-
-        foreach ([CommandBus::class, EventBus::class, QueryBus::class] as $gateway) {
+        foreach ([MessagingEntrypoint::class] as $gateway) {
             if ($interfaceToCall->getInterfaceType()->isCompatibleWith(TypeDescriptor::create($gateway))) {
-                return true;
+                return false;
             }
         }
 
-        return false;
+        return true;
     }
 }

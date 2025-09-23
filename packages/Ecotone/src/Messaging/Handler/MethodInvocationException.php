00fa2c3dc5260dd5f4dc93ea1a653f14906960fe
jlabedo <1191198+jlabedo@users.noreply.github.com>
2025-09-23 13:36:32 +0200

    Better error messages when fetching method arguments on function call

--- packages/Ecotone/src/Messaging/Handler/MethodInvocationException.php
+++ packages/Ecotone/src/Messaging/Handler/MethodInvocationException.php
@@ -1,8 +1,7 @@
 <?php
-
 declare(strict_types=1);
 
-namespace Ecotone\Messaging\Handler\Processor\MethodInvoker;
+namespace Ecotone\Messaging\Handler;
 
 use Ecotone\Messaging\MessagingException;
 

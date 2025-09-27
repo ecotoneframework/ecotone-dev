b98810cb2177cf473b8108774a8d450791b29ea8
Dariusz Gafka <dariuszgafka@gmail.com>
2025-09-27 12:43:03 +0200

    Instant retry and transaction interaction

---
+++ packages/PdoEventSourcing/tests/Integration/InstantRetryTransactionInteractionTest.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Ecotone\EventSourcing\Integration;
+
+use Ecotone\EventSourcing\EventSourcingConfiguration;
+use Ecotone\Lite\EcotoneLite;
+use Ecotone\Messaging\Config\ModulePackageList;
+use Ecotone\Messaging\Config\ServiceConfiguration;
+use Ecotone\Messaging\Support\ConcurrencyException;
+use Ecotone\Modelling\Config\InstantRetry\InstantRetryConfiguration;
+use Enqueue\Dbal\DbalConnectionFactory;
+use Test\Ecotone\EventSourcing\EventSourcingMessagingTestCase;
+use Test\Ecotone\EventSourcing\Fixture\InstantRetry\Aggregate\Customer;
+use Test\Ecotone\EventSourcing\Fixture\InstantRetry\AggregateMessages\RegisterCustomer;
+use Test\Ecotone\EventSourcing\Fixture\InstantRetry\AggregateMessages\CustomerRegistered;
+use Test\Ecotone\EventSourcing\Fixture\InstantRetry\EventsConverter;
+use Test\Ecotone\EventSourcing\Fixture\InstantRetry\TestRetryLogger;
+
+final class InstantRetryTransactionInteractionTest extends EventSourcingMessagingTestCase
+{
+    public function test_retry_happens_inside_aborted_transaction_with_prooph_conflict(): void
+    {
+        $logger = new TestRetryLogger();
+        $ecotone = EcotoneLite::bootstrapFlowTestingWithEventStore(
+            classesToResolve: [Customer::class, RegisterCustomer::class, CustomerRegistered::class, EventsConverter::class],
+            containerOrAvailableServices: [
+                DbalConnectionFactory::class => self::getConnectionFactory(),
+                new EventsConverter(),
+                'logger' => $logger,
+            ],
+            configuration: ServiceConfiguration::createWithDefaults()
+                ->withEnvironment('prod')
+                ->withSkippedModulePackageNames(ModulePackageList::allPackagesExcept([
+                    ModulePackageList::EVENT_SOURCING_PACKAGE,
+                    ModulePackageList::DBAL_PACKAGE,
+                    ModulePackageList::CORE_PACKAGE,
+                ]))
+                ->withExtensionObjects([
+                    EventSourcingConfiguration::createWithDefaults(),
+                    \Ecotone\Dbal\Configuration\DbalConfiguration::createWithDefaults()->withTransactionOnCommandBus(true),
+                    InstantRetryConfiguration::createWithDefaults()
+                        ->withCommandBusRetry(isEnabled: true, retryTimes: 1, retryExceptions: [ConcurrencyException::class]),
+                ]),
+            pathToRootCatalog: __DIR__ . '/../../',
+            runForProductionEventStore: true
+        );
+
+        $id = 'cust-1';
+
+        // Seed the aggregate via factory command
+        $ecotone->sendCommand(new RegisterCustomer($id));
+
+        // Trigger a conflicting append within transactional interceptor + instant retry
+        try {
+            $ecotone->sendCommand(new RegisterCustomer($id));
+            self::fail('Expected ConcurrencyException to be thrown');
+        } catch (ConcurrencyException $e) {
+            // ok
+        }
+
+        // Verify that retry has happened (logged by InstantRetryInterceptor)
+        self::assertTrue(
+            $logger->containsInfoSubstring('Trying to self-heal by doing instant try'),
+            'Expected InstantRetry to log a retry attempt'
+        );
+    }
+}
+

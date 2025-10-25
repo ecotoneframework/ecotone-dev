c59630e769f7f26a24e26bcd8bd34e8b34f641e7
jlabedo <1191198+jlabedo@users.noreply.github.com>
2025-10-25 08:49:33 +0200

    fix phpstan error on unknown WithoutDbalTransaction attribute in split testing

--- packages/Ecotone/src/Projecting/Config/ProjectingConsoleCommands.php
+++ packages/Ecotone/src/Projecting/Config/ProjectingConsoleCommands.php
@@ -7,7 +7,6 @@ declare(strict_types=1);
 
 namespace Ecotone\Projecting\Config;
 
-use Ecotone\Dbal\DbalTransaction\WithoutDbalTransaction;
 use Ecotone\Messaging\Attribute\ConsoleCommand;
 use Ecotone\Messaging\Attribute\ConsoleParameterOption;
 use Ecotone\Projecting\ProjectionRegistry;
@@ -38,7 +37,6 @@ class ProjectingConsoleCommands
     }
 
     #[ConsoleCommand('ecotone:projection:backfill')]
-    #[WithoutDbalTransaction]
     public function backfillProjection(string $name): void
     {
         if (! $this->registry->has($name)) {

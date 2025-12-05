104eb4d9527b596923266861e8f1aee65b452f3d
Dariusz Gafka <dgafka.mail@gmail.com>
2025-12-05 09:27:21 +0100

    Feat/cqrs user experience (#572)
    * feat: cqrs user experience
    
    * feat: cqrs inform when used with private method
    
    * do not register private methods
    
    * fixes

---
+++ packages/Ecotone/tests/Modelling/Fixture/EventRouting/PlaceOrder.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Ecotone\Modelling\Fixture\EventRouting;
+
+/**
+ * licence Apache-2.0
+ */
+final class PlaceOrder
+{
+    public function __construct(public string $orderId)
+    {
+    }
+}
+

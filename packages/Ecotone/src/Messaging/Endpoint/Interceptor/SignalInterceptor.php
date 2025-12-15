5a9fe9eb2b0a2db005c6cbeec4a8e7cbf329c60b
Jean de La Bédoyère <1191198+jlabedo@users.noreply.github.com>
2025-12-15 09:55:17 +0100

    reusable signal handler scope

--- packages/Ecotone/src/Messaging/Endpoint/Interceptor/SignalInterceptor.php
+++ packages/Ecotone/src/Messaging/Endpoint/Interceptor/SignalInterceptor.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Ecotone\Messaging\Endpoint\Interceptor;
 
-use Ecotone\Messaging\Config\ConfigurationException;
 use Ecotone\Messaging\Endpoint\ConsumerInterceptor;
 use Ecotone\Messaging\Endpoint\ConsumerInterceptorTrait;
+use RuntimeException;
 
 /**
  * Class SignalInterceptor
@@ -20,11 +20,7 @@ class SignalInterceptor implements ConsumerInterceptor
 {
     use ConsumerInterceptorTrait;
     private bool $shouldBeStopped = false;
-    private ?bool $pcntlAsyncSignalsOriginalState = null;
-    /**
-     * @var array<int, callable|int>|null
-     */
-    private ?array $beforeHandlers = null;
+    private ?SignalHandlerScope $signalHandlerScope = null;
 
     /**
      * @inheritDoc
@@ -32,35 +28,26 @@ class SignalInterceptor implements ConsumerInterceptor
     public function onStartup(): void
     {
         if (! extension_loaded('pcntl')) {
-            throw ConfigurationException::create('pcntl extension need to be loaded in order to catch system signals');
+            throw new RuntimeException('pcntl extension need to be loaded in order to catch system signals');
         }
+        if ($this->signalHandlerScope) {
+            throw new RuntimeException('Signal handler already registered');
+        }
+        $this->signalHandlerScope = new SignalHandlerScope();
+        $stopClosure = function () {
+            $this->shouldBeStopped = true;
+        };
 
-        $this->pcntlAsyncSignalsOriginalState = pcntl_async_signals();
-        pcntl_async_signals(true);
-
-        $this->beforeHandlers = [
-            SIGTERM => pcntl_signal_get_handler(SIGTERM),
-            SIGQUIT => pcntl_signal_get_handler(SIGQUIT),
-            SIGINT => pcntl_signal_get_handler(SIGINT),
-        ];
-
-        pcntl_signal(SIGTERM, $this->stopConsumer(...));
-        pcntl_signal(SIGQUIT, $this->stopConsumer(...));
-        pcntl_signal(SIGINT, $this->stopConsumer(...));
+        foreach ([SIGTERM, SIGQUIT, SIGINT] as $signal) {
+            $this->signalHandlerScope->register($signal, $stopClosure);
+        }
     }
 
     public function onShutdown(): void
     {
-        if ($this->beforeHandlers !== null) {
-            foreach ($this->beforeHandlers as $signal => $handler) {
-                pcntl_signal($signal, $handler);
-            }
-            $this->beforeHandlers = null;
-        }
-
-        if ($this->pcntlAsyncSignalsOriginalState !== null) {
-            pcntl_async_signals($this->pcntlAsyncSignalsOriginalState);
-            $this->pcntlAsyncSignalsOriginalState = null;
+        if ($this->signalHandlerScope) {
+            $this->signalHandlerScope->release();
+            $this->signalHandlerScope = null;
         }
     }
 
@@ -70,13 +57,5 @@ class SignalInterceptor implements ConsumerInterceptor
     public function shouldBeStopped(): bool
     {
         return $this->shouldBeStopped;
-    }
-
-    /**
-     * @param int $signal
-     */
-    public function stopConsumer(int $signal): void
-    {
-        $this->shouldBeStopped = true;
     }
 }

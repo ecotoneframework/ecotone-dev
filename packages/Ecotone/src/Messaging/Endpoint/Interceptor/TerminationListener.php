21e5a71f1d2a3939e49c98096a5b3b24be9c9138
Jean <1191198+jlabedo@users.noreply.github.com>
2025-12-22 18:14:27 +0100

    Handle signals (pcntl) when projecting (#585)
    * reusable signal handler scope
    
    * reusable signal handler scope
    
    * global signal termination handler
    
    * fix tests
    
    * fix tests
    
    * review
    
    * review
    
    * test message
    
    * introduce TerminationListener interface

---
+++ packages/Ecotone/src/Messaging/Endpoint/Interceptor/TerminationListener.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * licence Enterprise
+ */
+declare(strict_types=1);
+
+namespace Ecotone\Messaging\Endpoint\Interceptor;
+
+interface TerminationListener
+{
+    public function shouldTerminate(): bool;
+}

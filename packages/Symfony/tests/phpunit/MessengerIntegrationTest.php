dc1d6fa1b4e404483a0af818fedd252919588a6e
Dariusz Gafka <dariuszgafka@gmail.com>
2026-01-25 17:53:23 +0100

    fixes

--- packages/Symfony/tests/phpunit/MessengerIntegrationTest.php
+++ packages/Symfony/tests/phpunit/MessengerIntegrationTest.php
@@ -331,7 +331,7 @@ final class MessengerIntegrationTest extends WebTestCase
         $messaging->sendCommandWithRoutingKey('execute.example_command', $messagePayload, metadata: [
             MessageHeaders::DELIVERY_DELAY => (new DateTimeImmutable())->modify('+1 second'),
         ]);
-        sleep(1);
+        sleep(2);
         $messaging->run($channelName, ExecutionPollingMetadata::createWithTestingSetup());
         $this->assertCount(1, $messaging->sendQueryWithRouting('consumer.getMessages'));
     }

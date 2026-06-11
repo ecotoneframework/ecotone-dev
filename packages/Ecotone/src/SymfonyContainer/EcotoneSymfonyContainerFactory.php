cadba1f86212d330c94e91a793788f77cced4f39
Dariusz Gafka <dariuszgafka@gmail.com>
2026-06-11 09:51:35 +0200

    feat: add dumped container cache and symfony/dependency-injection requirement

--- packages/Ecotone/src/SymfonyContainer/EcotoneSymfonyContainerFactory.php
+++ packages/Ecotone/src/SymfonyContainer/EcotoneSymfonyContainerFactory.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Ecotone\SymfonyContainer;
 
 use Ecotone\Lite\InMemoryPSRContainer;
+use Ecotone\Messaging\Config\ConfigurationException;
 use Ecotone\Messaging\Config\Container\ContainerBuilder;
 use Ecotone\Messaging\Config\ServiceCacheConfiguration;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder as SymfonyContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface as SymfonyContainerInterface;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 
 /**
  * licence Apache-2.0
@@ -26,7 +28,54 @@ final class EcotoneSymfonyContainerFactory
         $builder->compile();
         $symfonyBuilder->compile();
 
+        if ($serviceCacheConfiguration->shouldUseCache()) {
+            self::dumpToCache($symfonyBuilder, $serviceCacheConfiguration);
+            return self::loadCached($serviceCacheConfiguration, $externalContainer)
+                ?? throw ConfigurationException::create("Failed to load dumped Ecotone container from {$serviceCacheConfiguration->getPath()}");
+        }
+
         return self::wrapWithExternalFallback($symfonyBuilder, $externalContainer);
+    }
+
+    public static function loadCached(
+        ServiceCacheConfiguration $serviceCacheConfiguration,
+        ?ContainerInterface $externalContainer = null,
+    ): ?EcotoneContainer {
+        $containerFile = self::containerFilePath($serviceCacheConfiguration);
+        $className = self::containerClassName($serviceCacheConfiguration);
+        if (! class_exists($className, false)) {
+            if (! file_exists($containerFile)) {
+                return null;
+            }
+            require_once $containerFile;
+        }
+
+        return self::wrapWithExternalFallback(new $className(), $externalContainer);
+    }
+
+    private static function dumpToCache(
+        SymfonyContainerBuilder $symfonyBuilder,
+        ServiceCacheConfiguration $serviceCacheConfiguration,
+    ): void {
+        $cacheDirectory = $serviceCacheConfiguration->getPath();
+        if (! is_dir($cacheDirectory)) {
+            mkdir($cacheDirectory, 0777, true);
+        }
+        $dumper = new PhpDumper($symfonyBuilder);
+        file_put_contents(
+            self::containerFilePath($serviceCacheConfiguration),
+            $dumper->dump(['class' => self::containerClassName($serviceCacheConfiguration)]),
+        );
+    }
+
+    private static function containerFilePath(ServiceCacheConfiguration $serviceCacheConfiguration): string
+    {
+        return $serviceCacheConfiguration->getPath() . DIRECTORY_SEPARATOR . 'ecotone_container.php';
+    }
+
+    private static function containerClassName(ServiceCacheConfiguration $serviceCacheConfiguration): string
+    {
+        return 'EcotoneCachedContainer_' . md5($serviceCacheConfiguration->getPath());
     }
 
     private static function wrapWithExternalFallback(
---
+++ packages/Ecotone/tests/SymfonyContainer/EcotoneSymfonyContainerFactoryCacheTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Ecotone\SymfonyContainer;
+
+use Ecotone\Lite\InMemoryPSRContainer;
+use Ecotone\Messaging\Config\Container\ContainerBuilder;
+use Ecotone\Messaging\Config\Container\Definition;
+use Ecotone\Messaging\Config\Container\Reference;
+use Ecotone\Messaging\Config\ServiceCacheConfiguration;
+use Ecotone\SymfonyContainer\EcotoneSymfonyContainerFactory;
+use Ecotone\Test\StubLogger;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * licence Apache-2.0
+ * @internal
+ */
+class EcotoneSymfonyContainerFactoryCacheTest extends TestCase
+{
+    public function test_it_loads_dumped_container_from_cache(): void
+    {
+        $cacheConfiguration = new ServiceCacheConfiguration($this->uniqueCacheDirectory(), true);
+        $builder = new ContainerBuilder();
+        $builder->replace('aService', new Definition(ACachedService::class, ['someName']));
+        EcotoneSymfonyContainerFactory::build($builder, $cacheConfiguration);
+
+        $loaded = EcotoneSymfonyContainerFactory::loadCached($cacheConfiguration);
+
+        self::assertNotNull($loaded);
+        self::assertEquals(new ACachedService('someName'), $loaded->get('aService'));
+    }
+
+    public function test_it_returns_null_when_no_dumped_container_exists(): void
+    {
+        $cacheConfiguration = new ServiceCacheConfiguration($this->uniqueCacheDirectory(), true);
+
+        self::assertNull(EcotoneSymfonyContainerFactory::loadCached($cacheConfiguration));
+    }
+
+    public function test_it_resolves_external_references_in_cache_loaded_container(): void
+    {
+        $cacheConfiguration = new ServiceCacheConfiguration($this->uniqueCacheDirectory(), true);
+        $builder = new ContainerBuilder();
+        $builder->replace('aService', new Definition(ACachedService::class, ['someName', new Reference('externallyDefined')]));
+        EcotoneSymfonyContainerFactory::build(
+            $builder,
+            $cacheConfiguration,
+            InMemoryPSRContainer::createFromAssociativeArray(['externallyDefined' => StubLogger::create()]),
+        );
+
+        $logger = StubLogger::create();
+        $loaded = EcotoneSymfonyContainerFactory::loadCached(
+            $cacheConfiguration,
+            InMemoryPSRContainer::createFromAssociativeArray(['externallyDefined' => $logger]),
+        );
+
+        self::assertSame($logger, $loaded->get('aService')->dependency);
+    }
+
+    private function uniqueCacheDirectory(): string
+    {
+        return sys_get_temp_dir() . '/ecotone_container_cache_test/' . uniqid('', true);
+    }
+}
+
+/**
+ * licence Apache-2.0
+ */
+class ACachedService
+{
+    public function __construct(public string $name, public mixed $dependency = null)
+    {
+    }
+}

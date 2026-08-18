69fc8ceeb80f39edb07271d0086142f79498dc5c
Dariusz Gafka <dariuszgafka@gmail.com>
2026-08-18 08:00:00 +0200

    feat: resolve typed config properties in Tempest's ConfigurationVariableService
    getByName('Some\Config\Class::property') now reads a public property off
    a config object registered in Tempest's container, in addition to the
    existing plain env-var lookup. Brings parameter('...') expression
    support in line with the Symfony and Laravel integrations.

--- packages/Tempest/tests/TempestConfigurationVariableServiceTest.php
+++ packages/Tempest/tests/TempestConfigurationVariableServiceTest.php
@@ -6,6 +6,8 @@ namespace Test\Ecotone\Tempest;
 
 use Ecotone\Tempest\TempestConfigurationVariableService;
 use PHPUnit\Framework\TestCase;
+use Tempest\Container\GenericContainer;
+use Test\Ecotone\Tempest\Fixture\Config\OrdersMultiTenancyConfig;
 
 /**
  * licence Apache-2.0
@@ -17,7 +19,7 @@ final class TempestConfigurationVariableServiceTest extends TestCase
     {
         putenv('ECOTONE_TEST_VAR=test-value');
 
-        $service = new TempestConfigurationVariableService();
+        $service = new TempestConfigurationVariableService(new GenericContainer());
 
         $this->assertTrue($service->hasName('ECOTONE_TEST_VAR'));
         $this->assertSame('test-value', $service->getByName('ECOTONE_TEST_VAR'));
@@ -25,9 +27,27 @@ final class TempestConfigurationVariableServiceTest extends TestCase
 
     public function test_returns_false_for_missing_env_variable(): void
     {
-        $service = new TempestConfigurationVariableService();
+        $service = new TempestConfigurationVariableService(new GenericContainer());
 
         $this->assertFalse($service->hasName('ECOTONE_NONEXISTENT_VAR_XYZ'));
         $this->assertNull($service->getByName('ECOTONE_NONEXISTENT_VAR_XYZ'));
+    }
+
+    public function test_reads_property_from_a_config_class_registered_in_the_container(): void
+    {
+        $container = new GenericContainer();
+        $container->singleton(OrdersMultiTenancyConfig::class, new OrdersMultiTenancyConfig('dynamicOrdersTopic'));
+
+        $service = new TempestConfigurationVariableService($container);
+
+        $this->assertTrue($service->hasName(OrdersMultiTenancyConfig::class . '::topicReferenceName'));
+        $this->assertSame('dynamicOrdersTopic', $service->getByName(OrdersMultiTenancyConfig::class . '::topicReferenceName'));
+    }
+
+    public function test_returns_false_for_a_config_class_property_that_does_not_exist(): void
+    {
+        $service = new TempestConfigurationVariableService(new GenericContainer());
+
+        $this->assertFalse($service->hasName(OrdersMultiTenancyConfig::class . '::missingProperty'));
     }
 }
